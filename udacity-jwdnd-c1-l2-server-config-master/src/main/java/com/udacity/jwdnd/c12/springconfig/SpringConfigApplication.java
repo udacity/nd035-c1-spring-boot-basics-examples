35b6f9e724cc19cf0996c87b5fb6a699ed2a0d2f
Sarai <sarai.gould@udacity.com>
2020-06-17 15:51:53 -0700

    New Folder

---
+++ udacity-jwdnd-c1-l2-server-config-master/src/main/java/com/udacity/jwdnd/c12/springconfig/SpringConfigApplication.java
@@ -0,0 +1,13 @@
+package com.udacity.jwdnd.c12.springconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConfigApplication.class, args);
+	}
+
+}
---
+++ udacity-jwdnd-c1-l2-server-config-master/src/test/java/com/udacity/jwdnd/c12/springconfig/SpringConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udacity.jwdnd.c12.springconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
