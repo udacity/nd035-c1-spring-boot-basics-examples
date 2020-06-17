979dfc6a1b610c1821670a6b769cffa3ece7a05a
Sarai <sarai.gould@udacity.com>
2020-06-17 15:59:21 -0700

    New Folder

---
+++ udacity-jwdnd-c1-l3-mvc-and-you-1-master/src/main/java/com/example/mvc_basics/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.example.mvc_basics.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/home")
+    public String getHomePage() {
+        return "home";
+    }
+
+}
