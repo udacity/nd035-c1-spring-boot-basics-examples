c5858c9eba705e96ebacb9303e0380f56a5f5cf2
Sarai <sarai.gould@udacity.com>
2020-06-17 15:44:52 -0700

    Upload l1 final review

---
+++ udacity-jwdnd-c1-l1-final-review-solution-master/src/main/java/com/udacity/jwdnd/c1/review/ReviewApplication.java
@@ -0,0 +1,13 @@
+package com.udacity.jwdnd.c1.review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewApplication.class, args);
+	}
+
+}
---
+++ udacity-jwdnd-c1-l1-final-review-solution-master/src/test/java/com/udacity/jwdnd/c1/review/ReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udacity.jwdnd.c1.review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
