cb9d0d4e8455af92c9dbdbc5c185791258d2fcc6
Sarai <sarai.gould@udacity.com>
2020-06-17 15:51:12 -0700

    New Folder

---
+++ udacity-jwdnd-c1-l2-spring-ioc-config-master/src/test/java/com/udacity/jwdnd/c1/l2/springiocconfigurationdemo/SpringIocConfigurationDemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.udacity.jwdnd.c1.l2.springiocconfigurationdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIocConfigurationDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
