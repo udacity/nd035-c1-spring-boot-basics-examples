28868dcd9181416439fe644ebca291fbe37657c3
Sarai <sarai.gould@udacity.com>
2020-07-29 10:44:08 -0700

    Added missing code file.
    MVC and You part 2

---
+++ udacity-jwdnd-c1-l3-mvc-and-you-2-master/src/main/java/com/example/mvc_basics/MvcBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.example.mvc_basics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcBasicsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcBasicsApplication.class, args);
+    }
+
+}
