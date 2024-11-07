7ecf186c0f97b13cc03cb2b1aaac5818216efafe
Neha Verma <nehbehl@gmail.com>
2024-11-07 17:27:58 +0530

    Update SecurityConfig.java
    Adding SecurityFilterChain implementation to make code compatible with springboot 3.3.5 and java 21.

--- udacity-jwdnd-c1-l4-spring-security-basics-master/src/main/java/com/udacity/jwdnd/spring_security_basics/config/SecurityConfig.java
+++ udacity-jwdnd-c1-l4-spring-security-basics-master/src/main/java/com/udacity/jwdnd/spring_security_basics/config/SecurityConfig.java
@@ -1,47 +1,43 @@
 package com.udacity.jwdnd.spring_security_basics.config;
 
-import com.udacity.jwdnd.spring_security_basics.mapper.UserMapper;
-import com.udacity.jwdnd.spring_security_basics.service.AuthenticationService;
-import com.udacity.jwdnd.spring_security_basics.service.HashService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.SecurityFilterChain;
 
-import java.util.ArrayList;
+import com.udacity.jwdnd.spring_security_basics.service.AuthenticationService;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
-    private AuthenticationService authenticationService;
+    final private AuthenticationService authenticationService;
 
     public SecurityConfig(AuthenticationService authenticationService) {
         this.authenticationService = authenticationService;
     }
+    
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+       
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(this.authenticationService);
-    }
+        http
+        .authorizeHttpRequests(requests -> requests
+            .requestMatchers("/signup", "/css/**", "/js/**").permitAll()
+            .anyRequest().authenticated()
+        )
+        
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/signup", "/css/**", "/js/**").permitAll()
-                .anyRequest().authenticated();
+        .formLogin(formLogin -> 
+            formLogin.loginPage("/login")
+                     .permitAll()
+                     .defaultSuccessUrl("/home", true)
+        )
+        .authenticationProvider(authenticationService);
 
-        http.formLogin()
-                .loginPage("/login")
-                .permitAll();
+    return http.build();
 
-        http.formLogin()
-                .defaultSuccessUrl("/home", true);
     }
 
 
