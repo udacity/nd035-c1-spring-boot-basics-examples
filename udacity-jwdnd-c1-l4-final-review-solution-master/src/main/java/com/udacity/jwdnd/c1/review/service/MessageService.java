5e1a87e8d7dafacac72acd6e442473cd2546fec5
Neha Verma <nehbehl@gmail.com>
2024-11-13 11:22:50 +0530

    Update MessageService.java
    Updated annotations import according to latest java 21.

--- udacity-jwdnd-c1-l4-final-review-solution-master/src/main/java/com/udacity/jwdnd/c1/review/service/MessageService.java
+++ udacity-jwdnd-c1-l4-final-review-solution-master/src/main/java/com/udacity/jwdnd/c1/review/service/MessageService.java
@@ -5,13 +5,13 @@ import com.udacity.jwdnd.c1.review.model.ChatForm;
 import com.udacity.jwdnd.c1.review.model.ChatMessage;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 import java.util.List;
 
 @Service
 public class MessageService {
 
-    private MessageMapper messageMapper;
+    private final MessageMapper messageMapper;
 
     public MessageService(MessageMapper messageMapper) {
         this.messageMapper = messageMapper;
@@ -26,15 +26,9 @@ public class MessageService {
         ChatMessage newMessage = new ChatMessage();
         newMessage.setUsername(chatForm.getUsername());
         switch (chatForm.getMessageType()) {
-            case "Say":
-                newMessage.setMessageText(chatForm.getMessageText());
-                break;
-            case "Shout":
-                newMessage.setMessageText(chatForm.getMessageText().toUpperCase());
-                break;
-            case "Whisper":
-                newMessage.setMessageText(chatForm.getMessageText().toLowerCase());
-                break;
+            case "Say" -> newMessage.setMessageText(chatForm.getMessageText());
+            case "Shout" -> newMessage.setMessageText(chatForm.getMessageText().toUpperCase());
+            case "Whisper" -> newMessage.setMessageText(chatForm.getMessageText().toLowerCase());
         }
         messageMapper.addMessage(newMessage);
     }
