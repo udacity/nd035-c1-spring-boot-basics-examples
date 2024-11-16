64b7dad072e1eb5185d82477a96eab12884876ff
Neha Verma <nehbehl@gmail.com>
2024-11-16 15:27:48 +0530

    Update SecurityConfig.java
    Updated securityconfig code for compatibility with  upgraded java 21 and springboot 3.3.5 upgrade

--- udacity-jwdnd-c1-l5-final-review-solution-master/src/main/java/com/udacity/jwdnd/c1/review/config/SecurityConfig.java
+++ udacity-jwdnd-c1-l5-final-review-solution-master/src/main/java/com/udacity/jwdnd/c1/review/config/SecurityConfig.java
@@ -1,42 +1,40 @@
 package com.udacity.jwdnd.c1.review.config;
 
-import com.udacity.jwdnd.c1.review.service.AuthenticationService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.udacity.jwdnd.c1.review.service.AuthenticationService;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    private AuthenticationService authenticationService;
+public class SecurityConfig {
+    
+    private final AuthenticationService authenticationService;
 
     public SecurityConfig(AuthenticationService authenticationService) {
         this.authenticationService = authenticationService;
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(this.authenticationService);
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    return http
+            .csrf(AbstractHttpConfigurer::disable)
+            .formLogin(httpForm ->{
+                httpForm.loginPage("/login").permitAll();
+                httpForm.defaultSuccessUrl("/chat");
+                
+            })            
+            .authorizeHttpRequests(registry ->{
+                registry.requestMatchers("/signup","/css/**","/js/**").permitAll();
+                registry.anyRequest().authenticated();
+            })
+            .authenticationProvider(authenticationService)
+            .build();
     }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/signup", "/css/**", "/js/**").permitAll()
-                .anyRequest().authenticated();
-
-        http.formLogin()
-                .loginPage("/login")
-                .permitAll();
-
-        http.formLogin()
-                .defaultSuccessUrl("/chat", true);
-    }
-
-
 }
 
 
