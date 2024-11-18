758358799d08f7cc57442e8907b965c0175d5c43
Neha Verma <nehbehl@gmail.com>
2024-11-18 17:38:41 +0530

    Update UserTestingApplicationTests.java
    Updated LocalServerPort library as per latest java changes.

--- udacity-jwdnd-c1-l5-junit-and-selenium-master/src/test/java/com/udacity/jwdnd/c1/l5/usertesting/UserTestingApplicationTests.java
+++ udacity-jwdnd-c1-l5-junit-and-selenium-master/src/test/java/com/udacity/jwdnd/c1/l5/usertesting/UserTestingApplicationTests.java
@@ -13,7 +13,7 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
